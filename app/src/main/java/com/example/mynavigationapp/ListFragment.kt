61313d5523290085dc24b0c891c24974200fa261
rohanimorteza <rohani.morteza@gmail.com>
2019-12-29 16:53:31 +0330

    02- Navigate and onSupportNavigationUp

--- app/src/main/java/com/example/mynavigationapp/ListFragment.kt
+++ app/src/main/java/com/example/mynavigationapp/ListFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_list.*
 
 
 class ListFragment : Fragment() {
@@ -16,6 +18,17 @@ class ListFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn.setOnClickListener {
+
+            Navigation.findNavController(it).navigate(R.id.action_listFragment_to_detailFragment)
+
+        }
+
     }
 
 
--- app/src/main/java/com/example/mynavigationapp/MainActivity.kt
+++ app/src/main/java/com/example/mynavigationapp/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.mynavigationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navControler : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navControler = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navControler)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navControler,null)
     }
 }
